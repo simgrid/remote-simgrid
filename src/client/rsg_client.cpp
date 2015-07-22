f5bed991a98a2a3475e7e62a0dbac46f34e79448
Martin Quinson <martin.quinson@loria.fr>
2015-07-22 22:27:06 +0200

    http ping/ping working

---
+++ src/client/rsg_client.cpp
@@ -0,0 +1,149 @@
+/*
+ * rsg_client.cpp
+ *
+ *  Created on: 22 juil. 2015
+ *      Author: mquinson
+ */
+
+#include <xbt.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <ctype.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+
+
+static int make_socket(const char*http_server, int port) {
+	/* get host info by name :*/
+	struct  hostent *hp;
+	struct  sockaddr_in     server;
+
+	if ((hp = gethostbyname( http_server )
+	)) {
+		memset((void *) &server,0, sizeof(server));
+		memmove((char *) &server.sin_addr, hp->h_addr, hp->h_length);
+		server.sin_family = hp->h_addrtype;
+		server.sin_port = (unsigned short) htons( port );
+	} else
+		xbt_die("CLI: Host not found: %s:%d\n",http_server,port);
+
+	/* create socket */
+	int sock =  socket(AF_INET, SOCK_STREAM, 0);
+
+	if (sock < 0)
+		perror("CLI: Cannot create a socket");
+	setsockopt(sock, SOL_SOCKET, SO_KEEPALIVE, 0, 0);
+
+	/* connect to server */
+	if (connect(sock, (const sockaddr*)&server, sizeof(server)) < 0) {
+		fprintf(stderr, "CLI: Cannot connect to %s:%d\n",http_server, port);
+		exit(1);
+	}
+
+	return sock;
+}
+
+/*
+ * read a line from file descriptor
+ * returns the number of bytes read. negative if a read error occurred
+ * before the end of line or the max.
+ * carriage returns (CR) are ignored.
+ *
+ * (C) 1998 Laurent Demailly. (Http put/get mini lib)
+ */
+static int http_read_line (
+		int fd,  /* file descriptor to read from */
+		char *buffer, /* placeholder for data */
+		int max /* max number of bytes to read */
+) { /* not efficient on long lines (multiple unbuffered 1 char reads) */
+	int n=0;
+	while (n<max) {
+		if (read(fd,buffer,1)!=1) {
+			n= -n;
+			break;
+		}
+		n++;
+		if (*buffer=='\015') continue; /* ignore CR */
+		if (*buffer=='\012') break;    /* LF is the separator */
+		buffer++;
+	}
+	*buffer=0;
+	return n;
+}
+
+
+static char *sock_POST(int sock, const char *url, const char*data) {
+	if (strchr(data,' ') || strchr(data,'\t') || strchr(data,'\n'))
+		xbt_die("Our protocol does not allow whitespaces in the data yet. hack hack...");
+
+	char buff[1024] = {0};
+//	char *header = bprintf("PUT /%s HTTP/1.0\015\012Content-Length: %zd\015\012User-Agent: C RSG Client\015\012\015\012",url,strlen(data));
+	char *header = bprintf("PUT /%s/%s HTTP/1.0\015\012User-Agent: C RSG Client\015\012\015\012",url,data);
+	int hlgr=strlen(header);
+	int ret;
+
+	/* send header */
+	if (write(sock,header,hlgr)!=hlgr)
+		xbt_die("CLI: Cannot send the headers: %s", strerror(errno));
+	free(header);
+
+	/* send data */
+	if (data) {
+		if (write(sock,data,strlen(data)) != (ssize_t)strlen(data))
+			xbt_die("CLI: Cannot send the data: %s", strerror(errno));
+		fprintf(stderr, "CLI: Sent: %s(%s)\n",url,data);
+	}
+
+	/* Get the answer */
+	if (http_read_line(sock, buff, 1024-1) <= 0)
+		xbt_die("Error while reading the answer (buff='%s')",buff);
+	fprintf(stderr, "CLI: Header of answer: %s\n",buff);
+
+	int http_ver;
+	ret = -1;
+	if (sscanf(buff,"HTTP/1.%d %03d",&http_ver, &ret)<=1)
+		xbt_die("CLI: Parse error on the answer's header.");
+
+	if (ret != 200)
+		xbt_die("CLI: The server answered code %d instead of 200. Bailing out\n",ret);
+
+	int data_length=-1;
+
+	while (1) {
+		if (http_read_line(sock, buff, 1024-1) <= 0)
+			xbt_die("Error while reading the answer");
+		fprintf(stderr, "CLI: seen hdr: %s\n",buff);
+		if (buff[0]=='\0') // Empty line: end of headers
+			break;
+		sscanf(buff,"Content-Length: %d",&data_length);
+	}
+	if (data_length<0)
+		xbt_die("CLI: Server did not put any Content-Length in its answer's headers.");
+
+	char *ans = xbt_new0(char,data_length);
+
+	int done;
+	for (done = 0; done < data_length;) {
+		int step = read(sock,ans+done,data_length-done);
+		if (step <= 0)
+			xbt_die("CLI: Something went wrong while reading the answer from the server. %d chars so far (%s).", done, ans);
+		done += step;
+	}
+
+	return ans;
+}
+
+int main(int argc, char **argv) {
+
+	int sock = make_socket("127.0.0.1",9000);
+	char *answer  = sock_POST(sock, "toto/tutu", "ze,data");
+
+	fprintf(stderr, "CLI: answer (len:%zd): %s\n", strlen(answer), answer);
+}
--- src/server.cpp
+++ src/server.cpp
@@ -1,82 +1,93 @@
-#include <microhttpd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include <errno.h>
 #include <signal.h>
 
-#include "simgrid/s4u.h"
+#include <microhttpd.h>
+#include <simgrid/s4u.h>
 
-#define PAGE "<html><head><title>libmicrohttpd demo</title>"\
-		"</head><body>libmicrohttpd demo %d</body></html>"
+#define pageOK "<html><body>SimGrid server up and running (or almost)</body></html>"
+
+static int display_headers(void *closure,
+		enum MHD_ValueKind kind,
+        const char *key, const char *value) {
+	fprintf(stderr,"SER: response hdr: %d, %s %s\n",kind,key, value);
+	return MHD_YES;
+}
 
 static int handle_request(void * cls,
 		struct MHD_Connection * connection,
 		const char * url, const char * method, const char * version, const char * upload_data, size_t * upload_data_size,
-		void ** ptr) {
-	static int dummy;
-	const char * page = PAGE;
+		void ** con_cls) {
+//	static int dummy; // To notice that we're called a second time
 	struct MHD_Response * response;
 	int ret;
 
-	if (&dummy != *ptr) {
-		if (0 != strcmp(method, "POST")) {
-			fprintf(stderr, "Method is not POST but %s\n", method);
-			return MHD_NO; /* unexpected method */
+	fprintf(stderr, "SER: Got an request to handle. URL='%s'; Data='%s' %s\n",url,upload_data,
+			(NULL == *con_cls)? "first time":"not first time");
+//	if (NULL == *con_cls) { /* First time we're called, just to check the headers */
+		if (0 != strcmp(method, "PUT")) {
+			fprintf(stderr, "SER: Ignoring a request using method %s (only PUT are served)\n\n", method);
+			return MHD_NO;      // Drop that connection, dude
 		}
-		/* The first time only the headers are valid, do not respond in the first round... */
-		*ptr = &dummy;  // note that we already validated the headers
-		return MHD_YES; // ask for the rest, beyond the headers
-	}
-	*ptr = NULL; /* clear context pointer: prepare to accept the headers only on next request  */
-	fprintf(stderr, "Got an request to handle. URL=%s; Data='%s'\n",url,upload_data);
+//		*con_cls = &dummy;  // we just validated the headers, no need to do it again and again
+//		return MHD_YES;         // ask for the rest, beyond the headers
+//	}
 
 	// Split process / action
-	char *process=strdup(url+1), *action = process;
+	char *process_name=strdup(url+1), *action = process_name;
 	while (*action !='/' && *action!='\0')
 		action++;
 	if (*action=='\0') {
-		fprintf(stderr, "Malformed URL: %s\n",url);
-		free(process);
+		fprintf(stderr, "SER: Malformed URL: %s\n",url);
+		free(process_name);
 		return MHD_NO;
 	}
 	*action='\0'; // final \0 of process name
 	action++;     // Move pointer onto the action name
 
-	fprintf(stderr,"Process: '%s', Action: '%s' \n",process,action);
 
 	// We can now answer that call
-	response = MHD_create_response_from_data(strlen(page),
-			(void*) page,
-			MHD_NO,
-			MHD_NO);
+	response = MHD_create_response_from_buffer(strlen(pageOK), (void*)pageOK, MHD_RESPMEM_PERSISTENT);
+	fprintf(stderr, "SER: Display headers\n");
+	MHD_get_response_headers(response, display_headers, NULL);
+	fprintf(stderr, "SER: Done with the headers\n");
+	if (!response)
+		xbt_die("Unable to generate response!");
 	ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
+	if (ret == MHD_NO)
+		xbt_die("Unable to post answer %p!",response);
+
+	fprintf(stderr,"SER: Process: '%s', Action: '%s', params: '%s', ret:%d \n",process_name,action,upload_data, ret);
+
 	MHD_destroy_response(response);
-	free(process);
+	free(process_name);
 	return ret;
 }
 
 static void catcher (int sig){}
 
 static void ignore_sigpipe () {
-  struct sigaction oldsig;
-  struct sigaction sig;
+	struct sigaction oldsig;
+	struct sigaction sig;
 
-  sig.sa_handler = &catcher;
-  sigemptyset (&sig.sa_mask);
-  sig.sa_flags = SA_INTERRUPT;
-  if (0 != sigaction (SIGPIPE, &sig, &oldsig))
-    fprintf (stderr, "Failed to install SIGPIPE handler: %s\n", strerror (errno));
+	sig.sa_handler = &catcher;
+	sigemptyset (&sig.sa_mask);
+	sig.sa_flags = SA_INTERRUPT;
+	if (0 != sigaction (SIGPIPE, &sig, &oldsig))
+		fprintf (stderr, "Failed to install SIGPIPE handler: %s\n", strerror (errno));
 }
 
-int main(int argc,
-		char ** argv) {
+int main(int argc, char ** argv) {
 	struct MHD_Daemon * d;
-	if (argc != 2) {
-		printf("Usage: %s PORT\n", argv[0]);
+	if (argc != 3) {
+		printf("Usage: %s platform.xml PORT\n", argv[0]);
 		return 1;
 	}
-	int port = atoi(argv[1]);
+	simgrid::s4u::Engine *e = new simgrid::s4u::Engine(&argc,argv);
+	e->loadPlatform(argv[1]);
+	int port = atoi(argv[2]);
 
 	ignore_sigpipe();
 	d = MHD_start_daemon(MHD_USE_THREAD_PER_CONNECTION,
@@ -88,7 +99,7 @@ int main(int argc,
 			MHD_OPTION_END);
 	if (d == NULL)
 		return 1;
-	fprintf(stderr,"Server started on port %d. Press enter to shut it down.\n", port);
+	fprintf(stderr,"SER. Started on port %d. Press enter to shut it down.\n", port);
 	getchar();
 	MHD_stop_daemon(d);
 	return 0;
