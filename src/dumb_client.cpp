6b254aebd007891eb8f69f8599bdcd4b561a16f4
Martin Quinson <martin.quinson@loria.fr>
2015-08-17 16:20:55 +0200

    remove unused headers from my example code

--- src/dumb_client.cpp
+++ src/dumb_client.cpp
@@ -3,13 +3,8 @@
 /* This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Affero Licence (see in file LICENCE).        */
 
-#include <rsg/actor.hpp>
-#include <xbt.h>
-
 #include <stdio.h>
-#include <unistd.h>
-
-#include "socket.h"
+#include <rsg/actor.hpp>
 
 int main(int argc, char **argv) {
 	simgrid::rsg::Actor &self = simgrid::rsg::Actor::self();
