af5418258601d09877aeaa326d14eaa9aa05d3d8
Millian Poquet <millian.poquet@inria.fr>
2018-02-27 15:46:56 +0100

    [test] global_get_clock should now work

--- tests/global/clock/main.cpp
+++ tests/global/clock/main.cpp
@@ -16,33 +16,33 @@ using boost::shared_ptr;
 using namespace ::simgrid;
 
 
-
-int actor(void *) {
-  rsg::MailboxPtr mbox = rsg::Mailbox::byName("toto");
-  XBT_INFO("get clock before receive : %f", rsg::getClock());
-  rsg::this_actor::recv(*mbox);
-  XBT_INFO("get clock after receive : %f", rsg::getClock());
-  rsg::this_actor::quit();
-  return 1;
+int actor(void *)
+{
+    rsg::MailboxPtr mbox = rsg::Mailbox::byName("toto");
+    XBT_INFO("%10.9f <- clock before receive (child)", rsg::getClock());
+    rsg::this_actor::recv(*mbox);
+    XBT_INFO("%10.9f <- clock after receive (child)", rsg::getClock());
+    rsg::this_actor::quit();
+    return 1;
 }
 
 int main() {
-  const char *msg = "Do you copy ? ";
-  rsg::HostPtr host1 = rsg::Host::by_name("host1");
+    const char *msg = "Do you copy ?";
+    rsg::HostPtr host1 = rsg::Host::by_name("host1");
 
-  XBT_INFO("get clock before execute : %f", rsg::getClock());
-  rsg::this_actor::execute(8095000000 * 2);
-  XBT_INFO("get clock after execute : %f", rsg::getClock());
-  
-  rsg::Actor *act = rsg::Actor::createActor("receiver" , host1 , actor, NULL);
-  
-  rsg::MailboxPtr mbox = rsg::Mailbox::byName("toto");
-  
-  XBT_INFO("get clock before send : %f", rsg::getClock());
-  rsg::this_actor::send(*mbox,msg, strlen(msg) + 1, 115476000);
-  XBT_INFO("get clock after send : %f", rsg::getClock());
-  act->join();
-  rsg::this_actor::quit();
-  return 0;
+    XBT_INFO("%10.9f <- clock before execute", rsg::getClock());
+    rsg::this_actor::execute(8095000000 * 2);
+    XBT_INFO("%10.9f <- clock after execute", rsg::getClock());
+
+    rsg::Actor *act = rsg::Actor::createActor("receiver" , host1 , actor, NULL);
+
+    rsg::MailboxPtr mbox = rsg::Mailbox::byName("toto");
+
+    XBT_INFO("%10.9f <- clock before send", rsg::getClock());
+    rsg::this_actor::send(*mbox,msg, strlen(msg) + 1, 115476000);
+    XBT_INFO("%10.9f <- clock after send", rsg::getClock());
+    act->join();
+    rsg::this_actor::quit();
+    return 0;
 }
 
