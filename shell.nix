f810c822ab4a0f9bd115d167f8e627d058458d7b
Millian Poquet <millian.poquet@inria.fr>
2018-07-16 15:03:10 +0200

    [nix] use 2018-03 nixpkgs (thrift 0.11.0)

--- shell.nix
+++ shell.nix
@@ -1,6 +1,11 @@
-with import (
-    fetchTarball
-    "https://gitlab.inria.fr/vreis/datamove-nix/repository/master/archive.tar.gz")
-    { };
+let
+  pkgs = import
+    ( fetchTarball "https://github.com/NixOS/nixpkgs/archive/18.03.tar.gz") {};
+  dmpkgs = import
+    #( fetchTarball "https://gitlab.inria.fr/vreis/datamove-nix/repository/master/archive.tar.gz")
+    # A local version can also be used:
+    ~/datamovepkgs
+  { inherit pkgs; };
+in
 
-remote_simgrid_dev
+dmpkgs.remote_simgrid_dev
