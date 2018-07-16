8c1627c5a5fd98ba69c881e3634295244ce7675c
Millian Poquet <millian.poquet@inria.fr>
2018-07-16 15:26:11 +0200

    [nix] oops: use master rather than local clone

--- shell.nix
+++ shell.nix
@@ -2,9 +2,9 @@ let
   pkgs = import
     ( fetchTarball "https://github.com/NixOS/nixpkgs/archive/18.03.tar.gz") {};
   dmpkgs = import
-    #( fetchTarball "https://gitlab.inria.fr/vreis/datamove-nix/repository/master/archive.tar.gz")
+    ( fetchTarball "https://gitlab.inria.fr/vreis/datamove-nix/repository/master/archive.tar.gz")
     # A local version can also be used:
-    ~/datamovepkgs
+    #~/datamovepkgs
   { inherit pkgs; };
 in
 
