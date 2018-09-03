5f63bc0f8c4009490674dafe7b342b0734029aaa
Millian Poquet <millian.poquet@inria.fr>
2018-09-03 16:06:33 +0200

    [misc] datamovepkgs -> kapack

--- shell.nix
+++ shell.nix
@@ -1,11 +1,11 @@
 let
   pkgs = import
     ( fetchTarball "https://github.com/NixOS/nixpkgs/archive/18.03.tar.gz") {};
-  dmpkgs = import
-    ( fetchTarball "https://gitlab.inria.fr/vreis/datamove-nix/repository/master/archive.tar.gz")
+  kapack = import
+    ( fetchTarball "https://github.com/oar-team/kapack/archive/master.tar.gz")
     # A local version can also be used:
-    #~/datamovepkgs
+    #~/kapack
   { inherit pkgs; };
 in
 
-dmpkgs.remote_simgrid_dev
+kapack.remote_simgrid_dev
