a1cd604231576e425d6acc2dc63fa3094b7c676e
Millian Poquet <millian.poquet@inria.fr>
2019-06-05 20:03:33 +0200

    [code] main -> serve

--- src/rsg/main.cpp
+++ src/rsg/main.cpp
@@ -4,13 +4,15 @@
 
 #include <docopt/docopt.h>
 
+#include "serve.hpp"
+
 int main(int argc, char * argv[])
 {
     static const char usage[] =
 R"(Remote SimGrid command-line tool.
 
 Usage:
-  rsg serve [options]
+  rsg serve <platform-file> [--port=<port>]
   rsg add-actor <vhost> [options] [--] <command> [<command-args>...]
   rsg start [options]
   rsg status [options]
@@ -18,16 +20,28 @@ Usage:
   rsg --help
 
 Options:
-  -h --hostname <host>  Server's TCP hostname [default: 127.0.0.1].
+  -h --hostname <host>  Server's hostname [default: 127.0.0.1].
   -p --port <port>      Server's TCP port [default: 35000].
 )";
 
+    // Parse CLI arguments.
     auto args = docopt::docopt(usage, { argv + 1, argv + argc }, true);
 
+    // Check argument validity
+    std::string platform_file = args["<platform-file>"].asString();
+    // TODO: check that file exists?
+    int port = args["--port"].asLong();
+    // TODO: check port bounds
+
+    // Debug printing, should be removed.
     std::cout << "Arguments:" << std::endl;
     for(auto const & arg : args) {
         std::cout << "  " << arg.first << ": " << arg.second << std::endl;
     }
+
+    // Manage subcommands.
+    if (args["serve"])
+        serve(platform_file, port);
 
     return 0;
 }
