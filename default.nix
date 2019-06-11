35d6fe614bb4ead48398f09ff21f9610527f84a4
Millian Poquet <millian.poquet@inria.fr>
2019-06-11 15:03:28 +0200

    [nix] coverage

--- default.nix
+++ default.nix
@@ -2,6 +2,7 @@
     (fetchTarball "https://github.com/oar-team/kapack/archive/master.tar.gz")
   {}
 , simgrid ? kapack.simgrid322_2
+, doCoverage ? true
 }:
 
 let
@@ -15,6 +16,14 @@ let
       src = ./.;
       nativeBuildInputs = [ pkgs.meson pkgs.pkgconfig pkgs.ninja ];
       buildInputs = [ simgrid kapack.docopt_cpp pkgs.boost pkgs.protobuf ];
+
+      mesonFlags = []
+        ++ pkgs.lib.optional doCoverage [ "-Db_coverage=true" ];
+      postInstall = pkgs.lib.optionalString doCoverage ''
+        mkdir -p $out/cov/rsg@exe $out/cov/rsg@sha
+        cp rsg@exe/*.gc{no,da} $out/cov/rsg@exe/
+        cp rsg@sha/*.gc{no,da} $out/cov/rsg@sha/
+      '';
 
       preConfigure = "rm -rf build";
 
