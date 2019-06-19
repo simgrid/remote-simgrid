c9051f0b25ef1e9eeea2ee655ab18d77df903de3
Millian Poquet <millian.poquet@inria.fr>
2019-06-19 02:37:00 +0200

    [test] coverage++

--- tests/clients/actor_whoami.cpp
+++ tests/clients/actor_whoami.cpp
@@ -9,8 +9,15 @@
 int main()
 {
     auto actor = rsg::Actor::self();
+    const int my_pid = actor->get_pid();
     printf("My actor is (pid=%d, name='%s'), located on Host(name='%s')\n",
-        actor->get_pid(), actor->get_name().c_str(), actor->get_host()->get_name().c_str());
+        my_pid, actor->get_name().c_str(), actor->get_host()->get_name().c_str());
+
+    if (my_pid != rsg::this_actor::get_pid())
+        printf("actor pid inconsistency!\n");
+
+    if (rsg::this_actor::is_maestro())
+        printf("I think I am maestro, but I am wrong.\n");
 
     return 0;
 }
