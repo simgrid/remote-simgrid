d12915133485d36ad45cf6f11b49afbfda133b66
Millian Poquet <millian.poquet@inria.fr>
2019-06-21 23:23:06 +0200

    [rsg] avoid a copy

--- src/rsg/simple_commands.cpp
+++ src/rsg/simple_commands.cpp
@@ -13,7 +13,7 @@
 int add_actor(const std::string & server_hostname, int server_port,
     const std::string & actor_name, const std::string & vhost_name,
     bool autoconnect, const std::string & command_to_run,
-    const std::vector<std::string> command_args)
+    const std::vector<std::string> & command_args)
 {
     try
     {
--- src/rsg/simple_commands.hpp
+++ src/rsg/simple_commands.hpp
@@ -6,7 +6,7 @@
 int add_actor(const std::string & server_hostname, int server_port,
     const std::string & actor_name, const std::string & vhost_name,
     bool autoconnect, const std::string & command_to_run,
-    const std::vector<std::string> command_args);
+    const std::vector<std::string> & command_args);
 int kill(const std::string & server_hostname, int server_port,
     const std::string & reason);
 int start(const std::string & server_hostname, int server_port);
