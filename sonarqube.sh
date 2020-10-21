1a18a21894d773d022395b4d6d1cabfb78dde844
Martin Quinson <martin.quinson@ens-rennes.fr>
2020-10-21 08:41:25 +0200

    use HTTPS on maven, bc it now fails with HTTP

--- sonarqube.sh
+++ sonarqube.sh
@@ -17,7 +17,7 @@ installSonarQubeScanner() {
   export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION
   rm -rf $SONAR_SCANNER_HOME
   mkdir -p $SONAR_SCANNER_HOME
-  curl -sSLo $HOME/.sonar/sonar-scanner.zip http://repo1.maven.org/maven2/org/sonarsource/scanner/cli/sonar-scanner-cli/$SONAR_SCANNER_VERSION/sonar-scanner-cli-$SONAR_SCANNER_VERSION.zip
+  curl -sSLo $HOME/.sonar/sonar-scanner.zip https://repo1.maven.org/maven2/org/sonarsource/scanner/cli/sonar-scanner-cli/$SONAR_SCANNER_VERSION/sonar-scanner-cli-$SONAR_SCANNER_VERSION.zip
   unzip $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
   chmod +x $SONAR_SCANNER_HOME/bin/sonar-scanner
   rm $HOME/.sonar/sonar-scanner.zip
